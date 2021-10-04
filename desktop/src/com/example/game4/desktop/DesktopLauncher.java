2078046ce42c6eee8b8173a7eb06dd67639edaeb
srally <srally1@binghamton.edu>
2021-10-04 10:22:07 -0400

    commit

---
+++ desktop/src/com/example/game4/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.example.game4.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.example.game4.Game4;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game4(), config);
+	}
+}
